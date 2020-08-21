0ccc314c5938fe690f0235c4e7d4eed9fc423924
Júlio Moura <juliomourajom@gmail.com>
2020-08-21 18:22:09 -0300

    :wrench: Configure security class

---
+++ src/main/java/com/julio/authservice/config/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.julio.authservice.config.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}

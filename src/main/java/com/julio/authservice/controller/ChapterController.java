1e359609fabdcf0ef2983bf58c21aceba7cc63d1
Júlio Moura <juliomourajom@gmail.com>
2020-09-08 14:32:12 -0300

    :bug: Return created for posting, and map id with path variable

--- src/main/java/com/julio/authservice/controller/ChapterController.java
+++ src/main/java/com/julio/authservice/controller/ChapterController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,11 +27,11 @@ public class ChapterController {
     @PostMapping
     public ResponseEntity<ChapterDTO> createChapter(@RequestBody ChapterDTOCreation chapterCreation) {
         ChapterDTO chapter = new ChapterDTO(chapterService.create(chapterCreation.convertToModel()));
-        return ResponseEntity.ok(chapter);
+        return new ResponseEntity<ChapterDTO>(chapter, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteChapter(@PathParam("id") Long id) {
+    public ResponseEntity<Void> deleteChapter(@PathVariable("id") Long id) {
         chapterService.deleteById(id);
         return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }

fedf87b7d4db63c0d39ea930df658115cdf60c92
Júlio Moura <juliomourajom@gmail.com>
2020-09-24 10:06:15 -0300

    :white_check_mark: Test pagination

--- src/test/java/com/julio/authservice/service/ChapterServiceTest.java
+++ src/test/java/com/julio/authservice/service/ChapterServiceTest.java
@@ -11,6 +11,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -51,13 +55,14 @@ class ChapterServiceTest {
     void validateFindAllChapters() {
         Chapter chapterOne = new Chapter("Java");
         Chapter chapterTwo = new Chapter("Ruby");
+        Pageable pageable = PageRequest.of(0, 2);
         List<Chapter> chaptersMocked = Arrays.asList(chapterOne, chapterTwo);
+        Page<Chapter> page = new PageImpl(chaptersMocked);
 
-        Mockito.when(chapterRepository.findAll()).thenReturn(chaptersMocked);
+        Mockito.when(chapterRepository.findAll(pageable)).thenReturn(page);
 
-        List<Chapter> chapters = chapterService.findAll();
-
-        assertEquals(2, chapters.size());
+        Page<Chapter> chapters = chapterService.findAll(0, 2);
+        assertEquals(2, chapters.getTotalElements());
     }
 
     @Test
